df6edd5dd267659099aace347c6c36a23c04d8d2
sshara <sshara.16@gmail.com>
2018-04-30 14:09:00 -0500

    Allll?

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
 	def configure_permitted_parameters
 
 	#	devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit({ roles: [] }, :name, :lastname, :email, :password, :password_confirmation, :asksecurityone, :answersecurityone, :asksecuritytwo, :answersecuritytwo) }
-		devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :lastname, :asksecurityone, :answersecurityone, :asksecuritytwo, :answersecuritytwo] )
+		devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :lastname, :sq1, :sa1, :sq2, :sa2] )
 
 	#	devise_parameter_sanitizer.permit(:sign_up) do |user|
   	#		user.permit(:name, :lastname, :email, :password, :password_confirmation, :asksecurityone, :answersecurityone, :asksecuritytwo, :answersecuritytwo)
